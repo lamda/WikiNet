ace8f19b5fe7eef3fd6b2fcb4e30b9704097b520
Daniel Lamprecht <daniel.lamprecht@tugraz.at>
2016-02-22 16:00:06 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+if __name__ == '__main__':
+    from datetime import datetime
+    start_time = datetime.now()
+    
+    # code goes here
+    
+    end_time = datetime.now()
+    print('Duration: {}'.format(end_time - start_time))
+
+
+
+
+
