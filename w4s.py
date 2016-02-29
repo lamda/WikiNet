a21dbf3edac39696da6547ce2b4e8de4e92c6cca
Daniel Lamprecht <daniel.lamprecht@gmx.at>
2016-02-29 22:07:25 +0100

    minor

--- w4s.py
+++ w4s.py
@@ -18,6 +18,18 @@ import random
 import re
 
 
+DATA_DIR = 'w4s'
+
+
+def get_title2id():
+    fpath = os.path.join('w4s', 'articles.txt')
+    with io.open(fpath, encoding='utf-8') as infile:
+        data = infile.read()
+
+    d = {f: i for i, f in enumerate(data.splitlines())}
+    return d
+
+
 def article_generator(offset=0, start=None):
     # get list of files
     fpath = os.path.join('w4s', 'articles.txt')
@@ -36,80 +48,6 @@ def article_generator(offset=0, start=None):
         with io.open(fpath, encoding='utf-8', errors='ignore') as infile:
             data = infile.read()
             yield f, data
-
-
-class Parser(object):
-    def __init__(self):
-        self.parentheses = 0
-
-    def parse(self, data):
-        self.parentheses = 0
-        do_print = True
-        for pos in range(len(data)):
-            if data[pos] == '|':
-                do_print = True
-            if do_print:
-                print(data[pos])
-            if data[pos] == '(':
-                self.parentheses += 1
-                print('    adding to par')
-            elif data[pos] == ')':
-                self.parentheses -= 1
-                print('    subtracting from par')
-            elif data[pos] == '<' and self.parentheses == 0:
-                lookahead = data[pos:pos+70]
-                if lookahead.startswith('<a href="../../wp/') or\
-                        lookahead.startswith('<a class="mw-redirect" href="href="../../wp/'):
-                    return pos
-        return -1
-
-
-link_regex = re.compile(r'(?:<a (?:class="mw-redirect" )?'
-                        'href="../../wp/[^/]+/(.+?)\.htm" title="[^"]+">'
-                        '.+?</a>)', re.DOTALL)
-parser = Parser()
-
-
-def get_first_link_alt(html):
-    # get rid of everything in () until the first link
-    # get rid of links in italics?
-    first_link = parser.parse(html)
-    if first_link != -1:
-        html = html[first_link:]
-    links = link_regex.findall(html)
-    print(links[0])
-    pdb.set_trace()
-
-    # links = link_regex.findall(html)
-    #
-    # html = 'Test (not a link: <a href="../../wp/p/Parisp1.htm" title="Paris">Paris</a>), blabla (also not a link: <a href="../../wp/p/Parisp2.htm" title="Paris">Paris</a>), but here comes a link: <a href="../../wp/p/link1.htm" title="Paris">Paris</a> and again (<a href="../../wp/p/link1.htm" title="link2p">Paris</a>) and again some text, and finally: <a href="../../wp/p/link1.htm" title="lastlink">Paris</a>'
-    # html = 'Test (not a link: <a href1>), blabla (also not a link: <a href2>), but here comes a link: <a href3> and again (<a href4>) and again some text, and finally: <a href5>'
-    # re_link = '(?:<a (?:class="mw-redirect" )?href="../../wp/[^/]+/(.+?)\\.htm" title="[^"]+">.+?</a>)'
-    # re_link = '(<a (class="mw-redirect" )?href="../../wp/[^/]+/(.+?)\\.htm" title="[^"]+">.+?</a>)'
-    # re.findall(r'(?:.*?(?:\(.*?\))?)*' + re_link + r'(?:.*?' + re_link + ')*', html)
-    #
-    # # http://stackoverflow.com/questions/12999419/python-re-findall-is-not-working-as-expected
-    # # I want to match all occurrences of the letter a, except for the first one if it is in parentheses
-    # e.g., 'b a b a c de a' --> ['a', 'a', 'a']
-    # e.g., 'b (a) b a c de a' --> ['a', 'a']
-
-
-def link_generator(html):
-    html = html.split('<!-- start content -->')[1].replace('\n', '')
-    html = html.split('<!-- end content -->')[0]
-    pdb.set_trace()
-    html = re.sub('<div.*?</div>', '', html)
-    pdb.set_trace()
-
-    # via http://stackoverflow.com/questions/35689153
-    without_paren = r'<a href="(?P<WITHOUT_PAREN>[^"]+)">'
-    with_paren = r'\(.*?<a href="(?P<WITH_PAREN>[^"]+)">.*?\)'
-    master_pattern = with_paren + '|' + without_paren  # try with_paren first
-    it = re.finditer(master_pattern, html)
-    # Drop matches in `it` until we hit the first without_paren match
-    # and yield every match from there onwards
-    for mo in itertools.dropwhile(lambda x: x.lastgroup == 'WITH_PAREN', it):
-        yield mo.group(mo.lastgroup)
 
 
 class WikipediaHTMLParser(HTMLParser.HTMLParser):
@@ -232,12 +170,26 @@ if __name__ == '__main__':
     from datetime import datetime
     start_time = datetime.now()
 
+    title2id = get_title2id()
+    title2links = {}
     parser = WikipediaHTMLParser(debug=False)
     # parser = WikipediaHTMLParser(debug=True)
     for idx, (title, html) in enumerate(article_generator()):
         print(title)
         parser.feed(html)
-        link = parser.get_data()[:1]
+        links = parser.get_data()[:20]
+        pdb.set_trace()
+        # TODO: problem is 'D%25C3%25A1l_Riata' (usage in articles) vs 'D%C3%A1l_Riata' (usage in article list)
+        title2links[title2id[title]] = [title2id[l] for l in links]
+
+    with io.open(os.path.join(DATA_DIR, 'top1links.tsv'), 'w',
+                 encoding='utf-8') as outfile:
+        for k in sorted(title2links):
+            outfile.write(k + '\t' + ';'.join(title2links[k]) + '\n')
+
+    # from main import Graph
+    # g = Graph(fname='top20links.tsv', use_sample=False, refresh=False, N=1)
+    # g.load_graph(refresh=False)
 
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
