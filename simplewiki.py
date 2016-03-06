8887e9639ab89031dd01cc3de75c314dbd90e053
Daniel Lamprecht <daniel.lamprecht@gmx.at>
2016-03-06 14:52:20 +0100

    reorganized code and removed redirect pages from graph

--- simplewiki.py
+++ simplewiki.py
@@ -18,6 +18,7 @@ import re
 
 from main import debug_iter, get_id_dict, get_redirect_dict,\
     get_resolved_redirects, Graph, read_pickle, check_files
+
 from crawler import Crawler
 
 
@@ -27,267 +28,13 @@ WIKI_CODE = 'simple'
 DUMP_DATE = '20160203'
 
 
-def crawl(data_dir, wiki_name, wiki_code, dump_date, recrawl_damaged=False):
-    with open(os.path.join(data_dir, 'id2title.obj'), 'rb') as infile:
-        id2title = pickle.load(infile)
-    pids = sorted(id2title)
-    Crawler(wiki_name, wiki_code, data_dir, dump_date, pids=pids,
-            recrawl_damaged=recrawl_damaged)
-
-
-class WikipediaHTMLParser(HTMLParser.HTMLParser):
-    def __init__(self, top_n, debug=False):
-        HTMLParser.HTMLParser.__init__(self)
-        self.top_n = top_n
-        self.found_links = 0
-        self.fed = []
-        self.tracking_link = False
-        self.parentheses_counter = 0
-        self.first_link_found = False
-        self.first_p_found = False
-        self.tracking_div = 0
-        self.div_counter = 0
-        self.table_counter = 0
-        self.tracking_table = 0
-        self.debug = debug
-        self.debug_found = False
-
-        self.skip_divs = [
-            'thumb',
-            'thumb tright',
-            'thumb tleft',
-            'boilerplate metadata'
-        ]
-
-        self.file_prefixes = {
-            # https://en.wikipedia.org/wiki/Help:Files
-            'User',
-            'Wikipedia',
-            'File',
-            'MediaWiki',
-            'Template',
-            'Help',
-            'Category',
-            'Portal',
-            'Book',
-            'Draft',
-            'Education_Program',
-            'TimedText',
-            'Module',
-            'Gadget',
-            'Gadget_definition',
-            'Topic',
-            'Special',
-            'Media'
-        }
-        file_prefixes_talk = {fp + '_talk' for fp in self.file_prefixes}
-        self.file_prefixes = self.file_prefixes | file_prefixes_talk
-
-    def reset(self):
-        self.found_links = 0
-        self.fed = []
-        self.tracking_link = False
-        self.parentheses_counter = 0
-        self.first_link_found = False
-        self.first_p_found = False
-        self.tracking_div = 0
-        self.div_counter = 0
-        self.table_counter = 0
-        self.tracking_table = 0
-        self.debug_found = False
-        HTMLParser.HTMLParser.reset(self)
-
-    def feed(self, data):
-        self.reset()
-        end_strings = [
-            '<h2><span class="mw-headline" id="References">',
-            '<h2><span class="mw-headline" id="Notes">',
-            '<h2><span class="mw-headline" id="Footnotes">',
-        ]
-        for end_string in end_strings:
-            data = data.split(end_string)[0]
-        for line in data.splitlines():
-            HTMLParser.HTMLParser.feed(self, line)
-            if self.found_links >= self.top_n:
-                break
-
-    def handle_starttag(self, tag, attrs):
-        # if self.debug and tag == 'a' and\
-        #     'Extraso' in ' '.join([a[1] for a in attrs]):
-        #     print('    -->', self.parentheses_counter)
-        #     print('    -->', self.tracking_table, self.table_counter,
-        #           self.tracking_div, self.div_counter)
-        #     pdb.set_trace()
-
-        if (tag == 'a' and self.div_counter == 0 and self.table_counter == 0)\
-                and (self.parentheses_counter == 0 or self.first_link_found)\
-                and self.first_p_found:
-            if self.debug:
-                print('a, 0', tag, attrs)
-            href = [a[1] for a in attrs if a[0] == 'href']
-            if href and href[0].startswith('/wiki/'):
-                a_init = href[0].split('/', 2)[-1].split(':')[0]
-                if a_init not in self.file_prefixes:
-                    self.fed.append(
-                        href[0].split('/', 2)[-1].split('#')[0]
-                    )
-                    self.tracking_link = True
-                    self.first_link_found = True
-                    self.found_links += 1
-
-        elif tag == 'div':
-            if self.debug:
-                print('div OPEN', tag, attrs)
-            aclass = [a[1] for a in attrs if a[0] == 'class']
-            if aclass and aclass[0] in self.skip_divs:
-                self.tracking_div += 1
-            if self.tracking_div:
-                self.div_counter += 1
-
-        elif tag == 'table':
-            if self.debug:
-                print('table OPEN', tag, attrs)
-            # aclass = [a[1] for a in attrs if a[0] == 'class']
-            # if aclass and 'infobox' in aclass[0]:
-            #     self.tracking_table += 1
-            self.tracking_table += 1
-            if self.tracking_table:
-                self.table_counter += 1
-
-        elif tag == 'p':
-            self.first_p_found = True
-
-    def handle_endtag(self, tag):
-        if tag == 'a' and self.tracking_link:
-            self.tracking_link = False
-        elif tag == 'div':
-            if self.debug:
-                print('div CLOSE')
-            if self.tracking_div > 0:
-                self.div_counter -= 1
-                if self.div_counter == 0:
-                    self.tracking_div = min(0, self.tracking_div-1)
-        elif tag == 'table':
-            if self.debug:
-                print('table CLOSE')
-            if self.tracking_table > 0:
-                self.table_counter -= 1
-                if self.table_counter == 0:
-                    self.tracking_table = min(0, self.tracking_table-1)
-
-    def handle_data(self, d):
-        # if not self.tracking_link:
-        #     d = d.strip('\t\x0b\x0c\r ')
-        #     if d:
-        #         self.fed.append(d + ' ')
-        # d = d.strip('\t\x0b\x0c\r \n')
-        # if not d:
-        #     return
-        # print(d)
-        # pdb.set_trace()
-        if self.tracking_div or self.tracking_table:
-            return
-        par_diff = d.count('(') - d.count(')')
-        self.parentheses_counter += par_diff
-        # if self.parentheses_counter > 0:
-        #     print(self.parentheses_counter, d)
-        # if 'Notable' in d:
-        #     self.debug_found = True
-        # if self.debug_found:
-        #     print(self.parentheses_counter, d)
-        # print(self.parentheses_counter, d)
-
-    def get_data(self):
-        return self.fed
-
-
-def resolve_redirects(links, title2id, title2redirect):
-    result = []
-    for link in links:
-        try:
-            result.append(title2id[title2redirect[link]])
-        except KeyError:
-            try:
-                result.append(title2id[link])
-            except KeyError:
-                # a link to an article that didn't exist at DUMP_DATE, but
-                # unfortunately is linked in the old revision retrieved via API
-                # print('       ', link, 'not found ----')
-                pass
-    return result
-
-
-def get_top_n_links_chunks(data_dir):
-    id2title = read_pickle(os.path.join(data_dir, 'id2title.obj'))
-    title2id = {v: k for k, v in id2title.items()}
-    title2redirect = read_pickle(os.path.join(data_dir, 'title2redirect.obj'))
-
-    file_names = [
-        f
-        for f in os.listdir(os.path.join(data_dir, 'html'))
-        if f.endswith('.obj')
-    ]
-    for fidx, file_name in enumerate(file_names):
-        print('\r', fidx+1, '/', len(file_names))
-        file_path = os.path.join(data_dir, 'html', file_name)
-        get_top_n_links(title2id, title2redirect, file_path)
-
-
-def get_top_n_links(title2id, title2redirect, file_path, top_n=20):
-    # extract the first n links
-    parser = WikipediaHTMLParser(top_n=top_n, debug=False)
-    df = pd.read_pickle(file_path)
-
-    parsed_links = []
-    for idx, row in df.iterrows():
-        # print(idx, pid, title, 'http://simple.wikipedia.org/wiki/' + title)
-        # if (idx % 1000) == 0:
-        #     print('\r', idx, end='')
-        if pd.isnull(row['redirects_to']):
-            parser.feed(row['content'])
-            # links = parser.get_data()[:20]
-            links = parser.get_data()
-            links = [l.replace('%25', '%') for l in links]  # fix double % encoding
-            # for l in links[:10]:
-            # for l in links[:10]:
-            #     print('   ', l)
-            # print('.........')
-            # for l in links[-10:]:
-            #     print('   ', l)
-            li = resolve_redirects(links, title2id, title2redirect)[:top_n]
-            # pdb.set_trace()
-            parsed_links.append([unicode(l) for l in li])
-        else:
-            parsed_links.append([])
-
-    df['links'] = parsed_links
-    pd.to_pickle(df, file_path)
-
-
-def combine_chunks(data_dir):
-    file_names = [
-        f
-        for f in os.listdir(os.path.join(data_dir, 'html'))
-        if f.endswith('.obj')
-    ]
-    with io.open(os.path.join(DATA_DIR, 'top20links.tsv'), 'w',
-                 encoding='utf-8') as outfile:
-        for fidx, file_name in enumerate(file_names):
-            print('\r', fidx, '/', len(file_names))
-            df = pd.read_pickle(os.path.join(data_dir, 'html', file_name))
-            for idx, row in df.iterrows():
-                outfile.write(
-                    unicode(row['pid']) + '\t' + ';'.join(row['links']) + '\n'
-                )
-
-
 if __name__ == '__main__':
     from datetime import datetime
     start_time = datetime.now()
 
     # get_id_dict(DATA_DIR, WIKI_NAME, DUMP_DATE)
 
-    # crawl(DATA_DIR, WIKI_NAME, WIKI_CODE, DUMP_DATE)
+    crawl(DATA_DIR, WIKI_NAME, WIKI_CODE, DUMP_DATE)
     # crawl(DATA_DIR, WIKI_NAME, WIKI_CODE, DUMP_DATE, recrawl_damaged=True)
 
     # get_resolved_redirects(DATA_DIR)
@@ -296,18 +43,18 @@ if __name__ == '__main__':
 
     # combine_chunks(DATA_DIR)
 
-    for n_val in [
-        1,
-        5,
-        10,
-        20
-    ]:
-        print('---------------- N = %d ----------------' % n_val)
-        g = Graph(data_dir=DATA_DIR, fname='top20links',
-                  use_sample=False, refresh=False, N=n_val)
-        g.load_graph(refresh=False)
-        g.compute_stats()
-        g.print_stats()
+    # for n_val in [
+    #     1,
+    #     5,
+    #     10,
+    #     20
+    # ]:
+    #     print('---------------- N = %d ----------------' % n_val)
+    #     g = Graph(data_dir=DATA_DIR, fname='top20links',
+    #               use_sample=False, refresh=False, N=n_val)
+    #     g.load_graph(refresh=False)
+    #     g.compute_stats()
+    #     g.print_stats()
 
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
