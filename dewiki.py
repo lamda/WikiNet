1e8b3c8cdb0e990254a51898c9174ee0adeb6424
Daniel Lamprecht <daniel.lamprecht@gmx.at>
2016-04-06 08:38:29 +0200

    minor

--- dewiki.py
+++ dewiki.py
@@ -16,25 +16,28 @@ if __name__ == '__main__':
     # wp.get_id_dict()
     # wp.crawl()
     # wp.crawl(recrawl_damaged=True)
-    wp.get_resolved_redirects()
-    wp.get_links('all')
+    # wp.get_resolved_redirects()
+    # wp.get_links(link_type='all', start=None, stop=81)
+    # wp.get_links(link_type='all', start=81, stop=162)
+    # wp.get_links(link_type='all', start=162, stop=243)
+    wp.get_links(link_type='all', start=243, stop=None)
     # wp.get_links('divs_tables')
-    wp.correct_bug()
-    wp.cleanup()
-
-    for n_val in [
-        1,
-        'first_p',
-        'lead',
-        'infobox',
-        'all',
-    ]:
-        print('---------------- N =', n_val, '----------------')
-        g = Graph(wiki_code=WIKI_CODE, N=n_val)
-        g.load_graph()
-        g.compute_stats()
-        # g.update_stats()
-        g.print_stats()
+    # wp.correct_bug()
+    # wp.cleanup()
+    #
+    # for n_val in [
+    #     1,
+    #     'first_p',
+    #     'lead',
+    #     'infobox',
+    #     'all',
+    # ]:
+    #     print('---------------- N =', n_val, '----------------')
+    #     g = Graph(wiki_code=WIKI_CODE, N=n_val)
+    #     g.load_graph()
+    #     g.compute_stats()
+    #     # g.update_stats()
+    #     g.print_stats()
 
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
